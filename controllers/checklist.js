62e18720352f913742d433f2cf11a983c1a9fe98
SoMin <thals0107@naver.com>
2022-10-30 12:14:12 +0900

    feat: checklist refactoring

--- controllers/checklist.js
+++ controllers/checklist.js
@@ -4,12 +4,36 @@ const _client = mongoClient.connect();
 
 const initState = {
   userId: 'test',
+  checked: [],
   items: [
-    { item: '0', checked: false },
-    { item: '1', checked: false },
-    { item: '2', checked: false },
-    { item: '3', checked: false },
-    { item: '4', checked: false },
+    {
+      title: '기본 짐싸기',
+      content: [
+        '의류',
+        '전자기기 챙기기',
+        '세안용품',
+        '상비약',
+        '신분증/면허증',
+        '필기구',
+        '마스크/손 소독제',
+      ],
+    },
+    {
+      title: '필수 준비물',
+      content: ['숙소'],
+    },
+    {
+      title: '트립로그에서 챙기기',
+      content: ['여행 일정짜기', '가계부 짜기'],
+    },
+    {
+      title: '통신/교통 준비',
+      content: ['여행지 교통편'],
+    },
+    {
+      title: '즐길거리 준비',
+      content: ['관광 정보 확인하기', '맛집 정보 확인하기'],
+    },
   ],
 };
 
@@ -37,10 +61,10 @@ const checkDB = {
     const client = await _client;
     const db = client.db('triplog').collection('checklist');
     const result = await db.updateOne(
-      { userId: 'test' },
+      { userId: 'test', 'items.title': item.title },
       {
         $addToSet: {
-          items: item,
+          'items.$.content': item.item,
         },
       }
       // { upsert: true }
@@ -56,7 +80,7 @@ const checkDB = {
     const client = await _client;
     const db = client.db('triplog').collection('checklist');
     const result = await db.updateOne(
-      { _id: el._id },
+      { userId: el.userId },
       { $set: { checked: el.checked } }
     );
     if (result.acknowledged) {
@@ -69,12 +93,12 @@ const checkDB = {
     const client = await _client;
     const db = client.db('triplog').collection('checklist');
     const result = await db.updateOne(
-      { userId: el.userId },
+      { userId: el.userId, 'items.title': el.title },
       // {
       //   items: { $elemMatch: { item: 'el.item' } },
       // },
       // { $unset: { item: 'el.item', checked: el.checked } }
-      { $pull: { items: { item: el.item } } }
+      { $pull: { 'items.$.content': el.item } }
     );
     if (result.acknowledged) {
       return '삭제 되었습니다.';
